92d5e334191db6b70dfaeefa1bf7652dcd7564a2
Ting Wang - Engineering <twang@twang-vm1.dwi.litle.com>
2014-08-27 16:32:12 -0400

    add samples and support verison 8.27

---
+++ samples/Capture/LitleForceCaptureTransaction.rb
@@ -0,0 +1,26 @@
+require_relative '../../lib/LitleOnline'
+#Force Capture
+force_capture_info = {
+  'merchantId' => '101',
+  'version'=>'8.8',
+  'reportGroup'=>'Planets',
+  'litleTxnId'=>'123456',
+  'orderId'=>'12344',
+  'amount'=>'106',
+  'orderSource'=>'ecommerce',
+  'card'=>{
+    'type'=>'VI',
+    'number' =>'4100000000000001',
+    'expDate' =>'1210'
+  }
+}
+response= LitleOnline::LitleOnlineRequest.new.force_capture(force_capture_info)
+ 
+#display results
+puts "Response: " + response.forceCaptureResponse.response
+puts "Message: " + response.forceCaptureResponse.message
+puts "Litle Transaction ID: " + response.forceCaptureResponse.litleTxnId
+
+if (!response.forceCaptureResponse.message.eql?'Approved')
+   raise ArgumentError, "LitleForceCaptureTransaction has not been Approved", caller
+end
